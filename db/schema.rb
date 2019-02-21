26c4a79409bf4ffcc366514bacc960becbd74d79
David Zhang <david.zhang2@shopify.com>
2019-02-21 17:03:10 -0500

    Add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 ActiveRecord::Schema.define(version: 2019_02_21_212103) do
 
   create_table "file_items", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.bigint "wave_id"
+    t.index ["wave_id"], name: "index_file_items_on_wave_id"
     t.string "name"
     t.string "format"
     t.decimal "size", precision: 10
     t.string "path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "wave_id"
-    t.index ["wave_id"], name: "index_file_items_on_wave_id"
   end
 
   create_table "waves", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Wave.create(
+  [
+    { code: 'text_text', text: 'Hello!' },
+    { code: 'text_file' },
+    { code: 'text_multiple_files' },
+  ]
+)
+
+FileItem.create(
+  [
+    { wave_id: 2, name: 'test_file', format: 'jpg', size: 0.7, path: 'uploads/2/test_file.jpg' },
+    { wave_id: 3, name: 'test_multiple_files_1', format: 'pdf', size: 2.1, path: 'uploads/3/test_multiple_files_1.pdf' },
+    { wave_id: 3, name: 'test_multiple_files_2', format: 'zip', size: 15, path: 'uploads/3/test_multiple_files_2.zip' },
+  ]
+)
