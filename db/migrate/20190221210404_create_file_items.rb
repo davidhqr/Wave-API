97fef1f1fbbb381ec2666f10412beb8d3037c1da
David Zhang <david.zhang2@shopify.com>
2019-02-21 16:39:06 -0500

    Add wave_id foreign key to FileItem

--- db/migrate/20190221210404_create_file_items.rb
+++ db/migrate/20190221210404_create_file_items.rb
@@ -1,7 +1,6 @@
 class CreateFileItems < ActiveRecord::Migration[5.2]
   def change
     create_table :file_items do |t|
-      t.references :wave_id, foreign_key: true
       t.string :name
       t.string :format
       t.decimal :size
