447cfaf8f411efce940679e689fe7261372004a2
David Zhang <david.zhang2@shopify.com>
2019-02-25 22:38:25 -0500

    Update ActiveStorage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'waves#index'
-  get '/waves/:code', to: 'waves#show'
+  get 'waves/:code', to: 'waves#show'
+  post 'waves', to: 'waves#create'
 end
